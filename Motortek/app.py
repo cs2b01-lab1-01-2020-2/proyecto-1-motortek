0957bb787ccf7f25dd1133a20069a7bd0797553f
cecharre <claudio.echarre@utec.edu.pe>
2020-10-21 22:20:26 -0500

    Added templates

--- Motortek/app.py
+++ Motortek/app.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 import psycopg2
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Quierochocolate9@localhost:5432/motortek'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/motortek'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -31,7 +31,7 @@ class Usuario_administrador(db.Model):
 
 class Auto(db.Model):
     __tablename__='auto'
-    id_usuario = db.Column(db.Integer, ForeignKey("usuario.id"), nullable=False)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario_cliente.id'), nullable=False)
     numplaca = db.Column(db.Integer, primary_key=True)
     modelo = db.Column(db.String(), nullable=False)
     foto = db.Column(db.LargeBinary, nullable=True)
@@ -45,12 +45,12 @@ class Servicio(db.Model):
     descripcion = db.Column(db.Integer, nullable=False)
     fecha_ingreso = db.Column(db.Date, nullable=False)
     fecha_entrega = db.Column(db.Date, nullable=False)
-    numplaca = db.Column(db.Integer, ForeignKey("auto.numplaca"), nullable=False)
+    numplaca = db.Column(db.Integer, db.ForeignKey("auto.numplaca"), nullable=False)
 
 class Mecanico(db.Model):
     __tablename__='mecanico'
     dni = db.Column(db.Integer, primary_key=True)
-    id_servicio = db.Column(db.Integer, ForeignKey("servicio.id"), nullable=False)
+    id_servicio = db.Column(db.Integer, db.ForeignKey("servicio.id"), nullable=False)
     nombre = db.Column(db.String(), nullable=False)
     apellido = db.Column(db.String(), nullable=False)
     sexo = db.Column(db.String(), nullable=False)
@@ -68,10 +68,18 @@ def index():
 @app.route('/login', methods=['GET'])
 def login():
     return render_template("login.html")
+
+@app.route('/login/signup', methods=['GET'])
+def login_sign():
+    return render_template("login.html")
     
 @app.route('/login_admin', methods=['GET'])
 def login_admin():
     return render_template("login_admin.html")
+
+
+
+
 
 @app.route('/register', methods=['GET'])
 def register():
