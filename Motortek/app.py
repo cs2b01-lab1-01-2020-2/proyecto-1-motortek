2b58e76530fa2f7c0924f29e5343be56c39d8f83
jose <jose.sanchez.a@utec.edu.pe>
2020-10-22 15:57:11 -0500

    script
    script

--- Motortek/app.py
+++ Motortek/app.py
@@ -7,11 +7,11 @@ from sqlalchemy.ext.declarative import declarative_base
 import psycopg2
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1214161820@localhost:5432/motortek'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Quierochocolate9@localhost:5432/motortek'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-connection = psycopg2.connect('dbname=motortek user=postgres password=1214161820 host=localhost')
+connection = psycopg2.connect('dbname=motortek user=postgres password=Quierochocolate9 host=localhost')
 cursor = connection.cursor()
 
 class Usuario_cliente(db.Model):
@@ -21,7 +21,7 @@ class Usuario_cliente(db.Model):
     apellido = db.Column(db.String(), nullable=False)
     sexo = db.Column(db.String(), nullable=False)
     contacto = db.Column(db.Integer, nullable=False)
-    email = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), nullable=False, unique=True)
     password = db.Column(db.String(), nullable=False)
 
 class Usuario_administrador(db.Model):
@@ -36,7 +36,7 @@ class Usuario_administrador(db.Model):
 
 class Auto(db.Model):
     __tablename__='auto'
-    id_usuario = db.Column(db.Integer, ForeignKey("usuario.id"), nullable=False)
+    email_usuario = db.Column(db.String(), ForeignKey("usuario_cliente.email"), nullable=False)
     numplaca = db.Column(db.Integer, primary_key=True)
     modelo = db.Column(db.String(), nullable=False)
     foto = db.Column(db.LargeBinary, nullable=True)
@@ -49,12 +49,12 @@ class Servicio(db.Model):
     descripcion = db.Column(db.String(300), nullable=False)
     fecha_ingreso = db.Column(db.Date, nullable=False)
     fecha_entrega = db.Column(db.Date, nullable=False)
+    mecanico = db.Column(db.Integer, ForeignKey("mecanico.dni"), nullable=False)
     numplaca = db.Column(db.Integer, ForeignKey("auto.numplaca"), nullable=False)
 
 class Mecanico(db.Model):
     __tablename__='mecanico'
     dni = db.Column(db.Integer, primary_key=True)
-    id_servicio = db.Column(db.Integer, ForeignKey("servicio.id"), nullable=False)
     nombre = db.Column(db.String(), nullable=False)
     apellido = db.Column(db.String(), nullable=False)
     sexo = db.Column(db.String(), nullable=False)
@@ -63,11 +63,11 @@ class Mecanico(db.Model):
 
 
 
-db.create_all()
+#db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.route('/admin_client', methods=['POST'])
 def admin_client():
@@ -134,6 +134,9 @@ def proceso_login():
         return redirect(url_for('login'))
     else:
         if(usuario.password == password):
+
+
+
             return render_template("client.html")
         else:
             return redirect(url_for('login'))
@@ -154,7 +157,27 @@ def proceso_login_admin():
         else:
             return redirect(url_for('login_admin'))
 
+@app.route('/register_auto', methods=['POST'])
+def register_auto():
+    placa = request.form['placa']
+    modelo = request.form['modelo']
+    correo = request.form['correo']
+    foto = request.form['imagen']
 
+    a = Auto(email_usuario=correo, placa=placa, modelo=modelo, foto=foto)
+    db.session.add(a)
+    db.session.commit()
+    return render_template("admin.html")
+
+@app.route('/register_servicio', methods=['POST'])
+def register_servicio():
+
+
+    return 
+
+@app.route('/register_mecanico', methods=['POST'])
+def register_mecanico():
+    return 
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
