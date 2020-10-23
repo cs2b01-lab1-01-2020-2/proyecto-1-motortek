258dda8e2bf6b82f4027993d09998858d60d7e3c
HarlopFlores <71047903+HarlopFlores@users.noreply.github.com>
2020-10-22 20:07:21 -0500

    Final

--- Motortek/app.py
+++ Motortek/app.py
@@ -14,6 +14,7 @@ migrate = Migrate(app, db)
 #connection = psycopg2.connect('dbname=motortek user=postgres password=Quierochocolate9 host=localhost')
 #cursor = connection.cursor()
 
+
 class Usuario_cliente(db.Model):
     __tablename__='usuario_cliente'
     id = db.Column(db.Integer, primary_key=True)
@@ -46,9 +47,9 @@ class Servicio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tipo = db.Column(db.String(), nullable=False)
     costo = db.Column(db.Integer, nullable=False)
-    descripcion = db.Column(db.String(300), nullable=False)
-    fecha_ingreso = db.Column(db.Date, nullable=False)
-    fecha_entrega = db.Column(db.Date, nullable=False)
+    descripcion = db.Column(db.String(), nullable=False)
+    fecha_ingreso = db.Column(db.String(), nullable=False)
+    fecha_entrega = db.Column(db.String(), nullable=False)
     mecanico = db.Column(db.Integer, ForeignKey("mecanico.dni"), nullable=False)
     numplaca = db.Column(db.Integer, ForeignKey("auto.numplaca"), nullable=False)
 
@@ -63,11 +64,18 @@ class Mecanico(db.Model):
 
 
 
-#db.create_all()
+db.create_all()
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/admin_client', methods=['POST'])
+def admin_client():
+    if(request.form['button1'] == 'Soy Cliente'):
+        return redirect(url_for('login'))
+    else:
+        return redirect(url_for('login_admin'))
 
 @app.route('/login', methods=['GET'])
 def login():
@@ -77,10 +85,9 @@ def login():
 def login_admin():
     return render_template("login_admin.html")
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['GET'])
 def register():
-    if(request.form['button1'] == 'Registrarse'):
-        return render_template("register.html")
+    return render_template("register.html")
 
 @app.route('/register_admin', methods=['POST'])
 def register_admin():
@@ -89,82 +96,140 @@ def register_admin():
 
 @app.route('/proceso_register', methods=['GET'])
 def proceso_register():
-    nombre = request.args.get("nombre")
-    apellido = request.args.get("apellido")
-    sexo = request.args.get("sexo")
-    contacto = request.args.get("contacto")
-    correo = request.args.get("email")
-    password = request.args.get("password")
-
-    u = Usuario_cliente(nombre = nombre, apellido= apellido, sexo=sexo, contacto=contacto, email=correo, password=password)
-    db.session.add(u)
-    db.session.commit()
-    return redirect(url_for('login'))
-
+    
+    try:
+        nombre = request.args.get("nombre")
+        apellido = request.args.get("apellido")
+        sexo = request.args.get("sexo")
+        contacto = request.args.get("contacto")
+        correo = request.args.get("email")
+        password = request.args.get("password")
+        error=False
+        u = Usuario_cliente(nombre = nombre, apellido= apellido, sexo=sexo, contacto=contacto, email=correo, password=password)
+        db.session.add(u)
+        db.session.commit()
+        return redirect(url_for('login'))
+    except:
+        db.session.rollback()
+        error=True
+        return render_template('register.html', error=error)
+    
 
 @app.route('/proceso_register_admin', methods=['GET'])
 def proceso_register_admin():
-    nombre = request.args.get("nombre")
-    apellido = request.args.get("apellido")
-    sexo = request.args.get("sexo")
-    contacto = request.args.get("contacto")
-    correo = request.args.get("email")
-    password = request.args.get("password")
-
-    u = Usuario_administrador(nombre=nombre, apellido= apellido, sexo=sexo, contacto=contacto, email=correo, password=password)
-    db.session.add(u)
-    db.session.commit()
-    return redirect(url_for('login_admin'))
+    try:
+        nombre = request.args.get("nombre")
+        apellido = request.args.get("apellido")
+        sexo = request.args.get("sexo")
+        contacto = request.args.get("contacto")
+        correo = request.args.get("email")
+        password = request.args.get("password")
+        error=False
+        u = Usuario_administrador(nombre=nombre, apellido= apellido, sexo=sexo, contacto=contacto, email=correo, password=password)
+        db.session.add(u)
+        db.session.commit()
+        return redirect(url_for('login_admin'))
+    except:
+        db.session.rollback()
+        error = True
+        return render_template('register_admin.html', error=error)
+    
 
 @app.route('/client', methods=['POST'])
-def client_sign():
-    correo = request.form.get('username')
-    contrasena = request.form.get('password')
-    data = Usuario_cliente.query.filter(Usuario_cliente.email==correo, Usuario_cliente.password==contrasena).first()
+def proceso_login():
+    correo = request.form['email']
+    password = request.form['password']
 
-    if data:
-        return redirect(url_for('plataform'))
-    else: 
-        return render_template('login.html', data=True)
+    usuario = Usuario_cliente.query.filter_by(email=correo).first()
+    
+    if(usuario is None):
+        return redirect(url_for('login'))
+    else:
+        if(usuario.password == password):
+            autos = Auto.query.with_entities(Auto.numplaca).filter_by(email_usuario=correo).all()
+            servicios = Servicio.query.filter(Servicio.numplaca.in_(autos)).all()
+            autos = Auto.query.filter_by(email_usuario=correo).all()
+            return render_template("client.html", client=usuario, autos=autos, servicios=servicios)
+        else:
+            return redirect(url_for('login'))
     
 
 @app.route('/admin', methods=['POST'])
 def proceso_login_admin():
-    correo = request.form.get('username')
-    contrasena = request.form.get('password')
-    data = Usuario_administrador.query.filter(Usuario_administrador.email==correo, Usuario_administrador.password==contrasena).first()
+    correo = request.form['email']
+    password = request.form['password']
 
-    if data:
-        return redirect(url_for('plataform'))
-    else: 
-        return render_template('login.html', data=True)
+    usuario = Usuario_administrador.query.filter_by(email=correo).first()
+    
+    if(usuario is None):
+        return redirect(url_for('login'))
+    else:
+        if(usuario.password == password):
+            return render_template("admin.html")
+        else:
+            return redirect(url_for('login_admin'))
 
-@app.route('/register_auto', methods=['POST'])
+@app.route('/register_auto', methods=['GET'])
 def register_auto():
-    placa = request.form['placa']
-    modelo = request.form['modelo']
-    correo = request.form['correo']
-    foto = request.form['imagen']
+    
+    try:
+        placa = request.args.get("placa")
+        modelo = request.args.get("modelo")
+        correo = request.args.get("correo")
+        placa = int(placa)
+        error=False
+        a = Auto(email_usuario=correo, numplaca=placa, modelo=modelo)
+        db.session.add(a)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        error = True
+    finally:
+        return render_template("admin.html", error=error)
+    
 
-    a = Auto(email_usuario=correo, placa=placa, modelo=modelo, foto=foto)
-    db.session.add(a)
-    db.session.commit()
-    return render_template("admin.html")
-
-@app.route('/register_servicio', methods=['POST'])
+@app.route('/register_servicio', methods=['GET'])
 def register_servicio():
+    
+    try:
+        tipo = request.args.get("servicio")
+        costo = request.args.get("costo")
+        descripcion = request.args.get("descripcion")
+        fecha_in= request.args.get("fecha_i")
+        fecha_out=request.args.get("fecha_e")
+        dni=request.args.get("dni")
+        placa=request.args.get("placa")
+        costo=int(costo)
+        dni= int(dni)
+        placa = int(placa)
+        error=False
+        s = Servicio(tipo=tipo, costo=costo, descripcion=descripcion, fecha_ingreso=fecha_in, fecha_entrega=fecha_out, numplaca=placa, mecanico=dni)
+        db.session.add(s)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        error = True
+    finally:
+        return render_template("admin.html", error=error)
+    
 
-
-    return 
-
-@app.route('/register_mecanico', methods=['POST'])
+@app.route('/register_mecanico', methods=['GET'])
 def register_mecanico():
-    return 
-
-@app.route('/plataform', methods=['GET'])
-def plataform():
-    return render_template("plataform.html")
-
+    try:
+        dni = request.args.get("dni")
+        nombre = request.args.get("nombre")
+        apellido = request.args.get("apellido")
+        sexo = request.args.get("sexo")
+        telefono = request.args.get("contacto")
+        error=False
+        m = Mecanico(dni=dni, nombre=nombre, apellido=apellido, sexo=sexo, contacto=telefono)
+        db.session.add(m)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        error = True
+    finally:
+        return render_template("admin.html", error=error)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
